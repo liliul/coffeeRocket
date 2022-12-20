7a2da472c56eacb6b638cef91c3ae415e10d40e7
liliu do carmo <32183622+liliul@users.noreply.github.com>
2022-12-20 13:14:50 -0300

    add: menu

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-var mudarImg = document.getElementById('mudarImg')
+const mudarImg = document.getElementById('mudarImg')
 
-function myFunction() {
-  var x = document.getElementById("navv");
-  if (x.style.display === 'none') {
-    x.style.display = "block";
+function handleMenu() {
+  let open = document.getElementById("openMenu");
+  
+  if (open.style.display === 'block') {
+
+      open.style.display = "none";
+      mudarImg.src='./assets/menu-buguer-open.svg'
+
+      document.body.style.overflow = 'visible'
+
+  } else {  
+    
+    open.style.display = "block";
     mudarImg.src='./assets/menu-buguer-close.svg'
-
-  } else {
-    x.style.display = "none";
-    mudarImg.src='./assets/menu-buguer-open.svg'
+    document.body.style.overflow='hidden'
   }
 }
